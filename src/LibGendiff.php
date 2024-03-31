7d6c953a1208d2b40f7b49d9e0c042f1aec7da21
olegserbat <olegserbat@yandex.ru>
2024-03-31 20:22:11 +0300

    step 3

--- src/LibGendiff.php
+++ src/LibGendiff.php
@@ -1,2 +1,39 @@
 <?php
+namespace Diff;
 
+function genDiff(string $PathFile1, string $PathFile2)
+{
+    $dataFile1 = file_get_contents($PathFile1);
+    $array1 = json_decode($dataFile1, true);
+    $dataFile2 = file_get_contents($PathFile2);
+    $array2 = json_decode($dataFile2, true);
+    ksort($array1);
+    ksort($array2);
+    //var_dump($array1);
+    $result = [];
+    foreach ($array1 as $key => $value) {
+        if ($value === true) {
+            $value = 'true';
+        } elseif ($value === false) {
+            $value = 'false';
+        }
+        if (array_key_exists($key, $array2) and $value === $array2[$key]) {
+            $result[] = "   {$key}: {$value}";
+        } else {
+            $result[] = " - {$key}: {$value}";
+        }
+    }
+    foreach ($array2 as $key => $value) {
+        if ($value === true) {
+            $value = 'true';
+        } elseif ($value === false) {
+            $value = 'false';
+        }
+        if (!array_key_exists($key, $array1) or $value !== $array1[$key]) {
+            $result[] = " + {$key}: {$value}";
+        }
+    }
+    return "\n" . "{" . "\n" . implode("\n", $result) . "\n" . "}";
+}
+
+//var_dump(genDiff('file1.json', 'file2.json'));
