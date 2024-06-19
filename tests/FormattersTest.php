49b32e2cc96ab50e85046e4ebccbcaa81a805c2f
olegserbat <olegserbat@yandex.ru>
2024-06-19 20:34:58 +0300

    step 8.3

--- tests/FormattersTest.php
+++ tests/FormattersTest.php
@@ -12,14 +12,13 @@ class FormattersTest extends TestCase
     {
         $path1 = __DIR__ . '/fixtures/file1.json';
         $path2 = __DIR__ . '/fixtures/file2.json';
-        $expectedLine = "
-{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
+        $expectedLine ="{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }";
         $this->assertEquals($expectedLine, genDiff($path1, $path2));
     }
