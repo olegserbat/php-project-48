c0be4fa2263ea3aff88f10cf76cefe8a09944836
olegserbat <olegserbat@yandex.ru>
2024-06-22 18:29:48 +0300

    step 8.15

--- src/Formatters.php
+++ src/Formatters.php
@@ -7,13 +7,10 @@ function formatters(array $diff, string $formatName = 'stylish')
     switch ($formatName) {
         case 'stylish':
             return "{" . printStylish($diff) . "\n" . "}";
-            break;
         case 'plain':
             return printPlain(addPath($diff));
-            break;
         case 'json':
             return json_encode($diff, JSON_PRETTY_PRINT);
-            break;
         default:
             return 'Format is not correct! Please, enter correct format';
     }
